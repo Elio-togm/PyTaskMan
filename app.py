5f24e8c19da514b83b7d9fd92eb6a1a6e0eb902c
Eliogames02 <Ayden.ray@icloud.com>
2025-04-18 15:16:26 -0700

    Made mark functions easier for users, and made list function able to take a status to sort by

--- app.py
+++ app.py
@@ -1,27 +1,25 @@
 import sys, time
 
 class TaskTracker:
-    def __init__(self):
-        self.tasks = {} # ID: [Task, Status, Creation_Date, Last_Modified]
+    def __init__(self, tasks={}):
+        self.tasks = tasks # ID: [Task, Status, Creation_Date, Last_Modified]
         self.__lowest_id__ = 1 # The starting ID of a task
         self.__all_ids__ = [0] # A list of all IDs in use
     
     def __help__(self): # Shows the help message which includes all available commands
-        print("\n-----------------------------------------------------------------------------")
+        print("\n---------------------------------------------------------------------------------------------------------------")
         print("\nPyTaskMan: A Task Tracker CLI program written in Python\n")
         print("Commands:")
-        print("add: Adds a task and returns its ID")
-        print("update (task_id) 'updated task': Updates a task")
-        print("delete (task_id): Deletes a task")
-        print("mark_in_progress (task_id): Marks a task as in progress")
-        print("mark_done (task_id): Marks a task as done")
-        print("mark_not_done (task_id): Marks a task as not done")
-        print("list (optional: status): Lists all tasks(if a status is provided, lists only tasks with that status)")
-        print("help: Shows this help message")
-        print("quit: Quits the program\n")
+        print("    add (name of task): Adds a task and returns its ID")
+        print("    update (task_id) 'updated task': Updates a task")
+        print("    delete (task_id): Deletes a task")
+        print("    mark (status: todo, doing, done) (task_id): Marks a task as TODO, IN PROGRESS, or DONE")
+        print("    list (optional: status): Lists all tasks(if a status is provided, lists only tasks with that status)")
+        print("    help: Shows this help message")
+        print("    quit: Quits the program\n")
         print("Optional Arguments (Only on startup of the program):")
-        print("-h, --help: Show this help message and exit\n")
-        return("-----------------------------------------------------------------------------\n")
+        print("    -h, --help: Show this help message and exit\n")
+        return("---------------------------------------------------------------------------------------------------------------\n")
     
     def __error_handler__(self, *args): # Handles errors if they exist
         pass
@@ -49,6 +47,18 @@ class TaskTracker:
         if self.__lowest_id__ > int(task_id):
             self.__lowest_id__ = int(task_id)
         return 'Task deleted successfully (ID: {})'.format(task_id)
+    
+    def mark(self, command, task_id):
+        if command.upper() == 'NOT_DONE' or command.upper() == 'N' or command.upper() == 'ND' or command.upper() == 'NOT DONE'\
+            or command.upper() == 'TODO' or command.upper() == 'T' or command.upper() == 'TODO':
+            return self.mark_todo(task_id) # Marks a task as not done
+        
+        elif command.upper() == 'IN_PROGRESS' or command.upper() == 'I' or command.upper() == 'IP' or command.upper() == 'IN PROGRESS' \
+            or command.upper() == 'INPROGRESS' or command.upper() == 'DOING' or command.upper() == 'DO':
+            return self.mark_in_progress(task_id) # Marks a task as in progress
+        
+        elif command.upper() == 'DONE' or command.upper() == 'D':
+            return self.mark_done(task_id) # Marks a task as done
 
     def mark_in_progress(self, task_id):
         self.tasks[int(task_id)][1] = "IN PROGRESS"
@@ -60,14 +70,21 @@ class TaskTracker:
         self.tasks[int(task_id)][3] = time.asctime(time.gmtime(time.time()))
         return 'Task marked done successfully (ID: {})'.format(task_id)
 
-    def mark_not_done(self, task_id):
-        self.tasks[int(task_id)][1] = "NOT DONE"
+    def mark_todo(self, task_id):
+        self.tasks[int(task_id)][1] = "TODO"
         self.tasks[int(task_id)][3] = time.asctime(time.gmtime(time.time()))
         return 'Task marked not done successfully (ID: {})'.format(task_id)
 
-    def list(self, command=None): #! NOT FULLY IMPLEMENTED
+    def list(self, command=None):
         if command == None:
             return dict(sorted(self.tasks.items())) # Returns a list of all tasks
+        elif command.upper() == "TODO" or command.upper() == "NOT DONE" or command.upper() == "T" or command.upper() == "N":
+            return dict(sorted({key: value for key, value in self.tasks.items() if value[1] == "NOT DONE"}.items())) # Returns a list of all tasks that are not done
+        elif command.upper() == "IN PROGRESS" or command.upper() == "I" or command.upper() == "IP" or command.upper() == "P" \
+            or command.upper() == "DOING" or command.upper() == "DO":
+            return dict(sorted({key: value for key, value in self.tasks.items() if value[1] == "IN PROGRESS"}.items())) # Returns a list of all tasks that are in progress
+        elif command.upper() == "DONE" or command.upper() == "D":
+            return dict(sorted({key: value for key, value in self.tasks.items() if value[1] == "DONE"}.items())) # Returns a list of all tasks that are done
 
     def help(self):
         return self.__help__() # Prints the help message
@@ -93,8 +110,8 @@ if __name__ == "__main__":
     command_number = 0 # Keeps track of the number of commands the user has entered
     max_command_number = float("inf") # Keeps track of the maximum number of commands the user can enter
     single_commmands = ['list', 'help', 'quit'] # A list of commands that can be entered without any arguments
-    double_commmands = ['add', 'delete', 'mark_in_progress', 'mark_done', 'mark_not_done'] # A list of commands that can be entered with 2 arguments
-    triple_commmands = ['update'] # A list of commands that can be entered with 3 arguments
+    double_commmands = ['add', 'delete', 'list'] # A list of commands that can be entered with 2 arguments
+    triple_commmands = ['update', 'mark'] # A list of commands that can be entered with 3 arguments
 
     while command_number < max_command_number:
         if command_number == 0: # Prints the help message the first time the user starts the program
@@ -110,11 +127,11 @@ if __name__ == "__main__":
         if user_command.strip(' ') == '':
             invalid_command() # Prints an error message for invalid arguments
 
-        command = user_command.split()[0]
+        command = user_command.lower().split()[0]
         valid_arguments_command = command in [method[0] for method in methods]
 
-        if user_command.strip(' ') in [method[0] for method in methods]: # Checks if the user entered a valid command without extra arguments: list, help, quit
-            print(pyTaskMan.__getattribute__(user_command.strip(' '))()) # Calls the function given by the command
+        if user_command.lower().strip(' ') in [method[0] for method in methods]: # Checks if the user entered a valid command without extra arguments: list, help, quit
+            print(pyTaskMan.__getattribute__(user_command.lower().strip(' '))()) # Calls the function given by the command
 
         elif valid_arguments_command and command in double_commmands: # Checks if the user entered a valid command with 2 arguments: add, delete, marks, lists
             print(pyTaskMan.__getattribute__(command)(" ".join(user_command.split()[1:]))) # Calls the 2-argument function given by the command
